87d76f5f79aaa6eb3a267768040f75d27ad1665d
shacky-me <meshackkiprono12@gmail.com>
2024-07-12 06:27:26 -0700

    Headings

--- markdown2html.py
+++ markdown2html.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-markdown2html.py: A script to convert Markdown files to HTML.
+markdown2html.py: A script to convert Markdown files to HTML, supporting headings.
 """
 
 import sys
@@ -9,7 +9,7 @@ import os
 
 def markdown_to_html(input_file, output_file):
     """
-    Converts a Markdown file to an HTML file.
+    Converts a Markdown file to an HTML file, parsing heading syntax.
 
     Arguments:
     input_file -- the name of the Markdown file to be converted
@@ -17,10 +17,17 @@ def markdown_to_html(input_file, output_file):
     """
     try:
         with open(input_file, 'r') as md_file:
-            md_content = md_file.read()
-        
-        html_content = md_content.replace('#', '<h1>').replace('\n', '<br>')
-        
+            html_content = ""
+            for line in md_file:
+                line = line.rstrip()
+                if line.startswith("#"):
+                    heading_level = len(line.split()[0])
+                    if 1 <= heading_level <= 6:
+                        line_content = line[heading_level:].strip()
+                        html_content += f"<h{heading_level}>{line_content}</h{heading_level}>\n"
+                else:
+                    html_content += line + "<br>\n"
+
         with open(output_file, 'w') as html_file:
             html_file.write(html_content)
 
